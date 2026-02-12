509c53964f36a4076d928295b9bedccdeb3f239b
callmehuda <mafatikhul.118@smk.belajar.id>
2026-02-12 21:18:49 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ set -e
 # Setup SSH
 echo 'root:huda' | chpasswd
 ssh-keygen -A
-echo -e "Port 22\nListenAddress 127.0.0.1\nPermitRootLogin yes\nPasswordAuthentication yes" | tee /etc/ssh/sshd_config
+printf "Port 22\nListenAddress 127.0.0.1\nPermitRootLogin yes\nPasswordAuthentication yes\n" | tee /etc/ssh/sshd_config
 mkdir -p /run/sshd
 chmod 755 /run/sshd
 
