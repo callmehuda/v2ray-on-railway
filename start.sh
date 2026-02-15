4e2a4cb072a5bc5ce06bc4baa99255d49fa26fbe
callmehuda <mafatikhul.huda118@smk.belajar.id>
2026-02-16 00:06:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ printf "Port 22\nListenAddress 127.0.0.1\nPermitRootLogin yes\nPasswordAuthentic
 mkdir -p /run/sshd
 chmod 755 /run/sshd
 apt update && apt upgrade -y
-apt install python3 python3-pip python3.12-venv python3-ensurepip fish -y
+apt install python3 python3-pip python3.12-venv fish -y
 python3 -m venv myvenv
 
 # Start sshd (background)
