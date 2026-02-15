59081ce68b11ecdeec0df168f080659b7866ac6f
callmehuda <mafatikhul.huda118@smk.belajar.id>
2026-02-16 00:00:22 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,9 +10,9 @@ ssh-keygen -A
 printf "Port 22\nListenAddress 127.0.0.1\nPermitRootLogin yes\nPasswordAuthentication yes\nSubsystem sftp internal-sftp\n" | tee /etc/ssh/sshd_config
 mkdir -p /run/sshd
 chmod 755 /run/sshd
-apt install python3 python3-pip -y
+apt update && apt upgrade -y
+apt install python3 python3-pip fish -y
 python3 -m venv myvenv
-apt install fish
 
 # Start sshd (background)
 /usr/sbin/sshd
